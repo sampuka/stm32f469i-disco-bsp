ec4ba8736ef1594f38a0e0b0812cdf3d97b24840
Mathias Ebbensgaard Jensen <matddi@gmail.com>
2023-09-10 15:39:57 +0200

    Support TIM2

--- include/bsp_devtemp.hpp
+++ include/bsp_devtemp.hpp
@@ -8,6 +8,8 @@ namespace bsp
 
 std::uint32_t get_tim2_counter();
 
-}
+void reset_tim2_interrupt();
+
+}  // namespace bsp
 
 #endif  // INCLUDE_BSP_DEVTEMP_HPP
--- src/bsp_devtemp.cpp
+++ src/bsp_devtemp.cpp
@@ -10,4 +10,10 @@ std::uint32_t get_tim2_counter()
 	return tim2.get_counter();
 }
 
+void reset_tim2_interrupt()
+{
+	tim2.reset_interrupt();
+	// tim2.disable();
+}
+
 }  // namespace bsp
