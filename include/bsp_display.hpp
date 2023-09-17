7f9391cd3c2ca889bc9e31ac32b1ea401f5a9f4e
Mathias Ebbensgaard Jensen <matddi@gmail.com>
2023-09-17 22:38:57 +0200

    LCD Display driver infancy

---
+++ include/bsp_display.hpp
@@ -0,0 +1,17 @@
+#ifndef INCLUDE_BSP_DISPLAY_HPP
+#define INCLUDE_BSP_DISPLAY_HPP
+
+namespace bsp
+{
+
+class Display
+{
+public:
+	Display() = default;
+
+	void power_on();
+};
+
+}  // namespace bsp
+
+#endif  // INCLUDE_BSP_DISPLAY_HPP
---
+++ src/bsp_display.cpp
@@ -0,0 +1,10 @@
+#include "bsp_display.hpp"
+
+namespace bsp
+{
+
+void Display::power_on()
+{
+}
+
+}  // namespace bsp
